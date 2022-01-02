2e38b5050f20f34f6499201fa16ac08d01965d8f
reuzun <moruka12@yandex.com>
2022-01-02 19:40:44 +0300

    Test added. Doc added. Snippets added.

--- src/test/java/ceng/estu/group2/shoebasketweb/ShoebasketwebApplicationTests.java
+++ src/test/java/ceng/estu/group2/shoebasketweb/ShoebasketwebApplicationTests.java
@@ -1,13 +1,97 @@
 package ceng.estu.group2.shoebasketweb;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.attributes;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
 class ShoebasketwebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
+    }
+
+
+    @Test
+    public void crudCreateExample() throws Exception {
+
+        this.mockMvc.perform(get("/api/models/random").param("limit", "1"))
+                .andExpect(status().isOk())
+                .andDo(document("tags-list-example",
+                        links(),
+                        responseFields(
+                                fieldWithPath("success").description("is true").optional(),
+                                fieldWithPath("message").description("Message of api answer.").optional(),
+                                fieldWithPath("data[].modelId").description("Id of the model.").optional(),
+                                fieldWithPath("data[].modelName").description("Name of the model.").optional(),
+                                fieldWithPath("data[].brandName").description("brand of the model.").optional(),
+                                fieldWithPath("data[].type").description("type of the model").optional(),
+                                fieldWithPath("data[].price").description("price of the model.").optional(),
+                                fieldWithPath("data[].colors").description("available colors of the model.").optional(),
+                                fieldWithPath("data[].sizes").description("available sizes of the model.").optional()
+
+                        ),
+                        requestParameters(parameterWithName("limit").description("Bring n random items. Default = 10"))
+                ));
+    }
 }
